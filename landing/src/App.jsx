927bb4b28b1ec40671ec8179ecee6823f8ca68be
Ravindu Aratchige <109760834+ravi-aratchige@users.noreply.github.com>
2025-05-16 01:49:47 +0530

    INIT: project setup with Tailwind and React-Router-Dom

---
+++ landing/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Home from './pages/home/Home';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                {/* Root route */}
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default App;
