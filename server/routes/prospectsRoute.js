6cf42c89212682940028632290dcd6dbdce7f0fe
Hasith Sandesh Vitanage <129611058+wtfHasi@users.noreply.github.com>
2025-05-16 18:57:41 +0530

    UPDATE: Alter route definitions

--- server/routes/prospectsRoute.js
+++ server/routes/prospectsRoute.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import { addProspect, getAllProspects } from '../controllers/prospectsController.js';
 
 const router = Router();
-router.post('/', addProspect);
-router.get('/', getAllProspects);
+router.post('/addProspect', addProspect);
+router.get('/getAllProspects', getAllProspects);
 
 export default router;
