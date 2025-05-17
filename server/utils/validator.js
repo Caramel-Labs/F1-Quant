f4852d4e544775dca93b51d3581c3806c55759db
Hasith Sandesh Vitanage <129611058+wtfHasi@users.noreply.github.com>
2025-05-17 14:49:12 +0530

    UPDATE: improved from basic

--- server/utils/validator.js
+++ server/utils/validator.js
@@ -1,4 +1,23 @@
+import validator from 'validator';
+
+const blockedDomains = [
+  'example.com',
+  'test.com',
+  'mailinator.com',
+  '10minutemail.com',
+  'tempmail.com',
+  'dispostable.com'
+];
+
 export function isValidEmail(email) {
-    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return re.test(email);
+  if (!validator.isEmail(email)) {
+    return false;
   }
+
+  const domain = email.split('@')[1].toLowerCase();
+  if (blockedDomains.includes(domain)) {
+    return false;
+  }
+
+  return true;
+}
